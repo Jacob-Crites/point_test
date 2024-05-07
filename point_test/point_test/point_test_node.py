234bc8e21a54a579b2e4ccb24332a74092cacf0a
Jacob-Crites <157927779+Jacob-Crites@users.noreply.github.com>
2024-05-06 19:38:10 -0500

    Update point_test_node.py
    Added multiple input states for PID to read over time

--- point_test/point_test/point_test_node.py
+++ point_test/point_test/point_test_node.py
@@ -7,7 +7,7 @@ import numpy as np
 from geometry_msgs.msg import Point
 
 #Constant publishing
-"""
+
 
 class Point_Test_Node(Node): #Node Class (just for program)
 
@@ -21,11 +21,56 @@ class Point_Test_Node(Node): #Node Class (just for program)
 
     def point_publish(self):
         fake_data = Point()
-        fake_data.x = float(5)
-        fake_data.z = float(3)
+        #State 1
+        fake_data.x = float(0.7)
+        fake_data.z = float(0)
         self.get_logger().info("Publishing X: " + str(fake_data.x) )
         self.get_logger().info("Publishing Z: " + str(fake_data.z) )
         self.my_point.publish(fake_data)
+        sleep(1)
+        #State 2
+        fake_data.x = float(1)
+        fake_data.z = float(0.5)
+        self.get_logger().info("Publishing X: " + str(fake_data.x) )
+        self.get_logger().info("Publishing Z: " + str(fake_data.z) )
+        self.my_point.publish(fake_data)
+        sleep(1)
+        #State 3
+        fake_data.x = float(0.8)
+        fake_data.z = float(-0.4)
+        self.get_logger().info("Publishing X: " + str(fake_data.x) )
+        self.get_logger().info("Publishing Z: " + str(fake_data.z) )
+        self.my_point.publish(fake_data)
+        sleep(1)
+        #State 4
+        fake_data.x = float(0.5)
+        fake_data.z = float(0.5)
+        self.get_logger().info("Publishing X: " + str(fake_data.x) )
+        self.get_logger().info("Publishing Z: " + str(fake_data.z) )
+        self.my_point.publish(fake_data)
+        sleep(1)
+        #State 5
+        fake_data.x = float(1)
+        fake_data.z = float(0)
+        self.get_logger().info("Publishing X: " + str(fake_data.x) )
+        self.get_logger().info("Publishing Z: " + str(fake_data.z) )
+        self.my_point.publish(fake_data)
+        sleep(1)
+        #State 6
+        fake_data.x = float(0.5)
+        fake_data.z = float(0)
+        self.get_logger().info("Publishing X: " + str(fake_data.x) )
+        self.get_logger().info("Publishing Z: " + str(fake_data.z) )
+        self.my_point.publish(fake_data)
+        sleep(1)
+        #State 7
+        fake_data.x = float(0.3)
+        fake_data.z = float(0)
+        self.get_logger().info("Publishing X: " + str(fake_data.x) )
+        self.get_logger().info("Publishing Z: " + str(fake_data.z) )
+        self.my_point.publish(fake_data)
+        sleep(2)
+    
             
 
 def main(args = None):
@@ -38,8 +83,8 @@ def main(args = None):
 if __name__ == '__main__':
     main()
 
-"""
 
+"""
 #One time publish
 def main(args = None):
     rclpy.init(args=args)
@@ -68,3 +113,4 @@ def main(args = None):
 
 if __name__ == '__main__':
     main()
+"""
